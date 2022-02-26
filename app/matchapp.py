108ea8db63be832de57e44cc6d0959834ef624ad
bluesy <alexmeng99@gmail.com>
2022-02-26 12:02:13 -0800

    Connected home & parent signup page to server.
    Changed href in registration.html to work via flask.

--- app/matchapp.py
+++ app/matchapp.py
@@ -5,4 +5,11 @@ app = Flask(__name__) #name of module
 #what we type into our browser to go to different pages
 @app.route("/") #home page
 def welcome():
-    return render_template('index.html') #name of home html file --> will change as front end is developed
+    return render_template('registration.html') #name of home html file --> will change as front end is developed
+
+@app.route("/parent") #parent sign up page
+def parent():
+    return render_template('parent.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
